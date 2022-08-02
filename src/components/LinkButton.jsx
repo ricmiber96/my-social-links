99b4daa6f211737bc614a9e1bf8433c51144097b
Ricardo Miralles <ricmiber@gmail.com>
2022-08-02 12:31:01 +0200

    Adding hover effect on button links

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -4,7 +4,7 @@ import { React } from 'react'
 export default function LinkButton ({ name, icon, bgColor, textColor, link }) {
   return (
     <a
-      className={`${textColor} ${bgColor} hover:bg-gray/90 font-medium rounded-lg text-lg w-96 px-6 py-4 flex justify-around mb-2 shadow-lg cursor-pointer mb-4`}
+      className={`${textColor} ${bgColor} hover:bg-gray/90 hover:scale-110 transform transition duration-500 font-medium rounded-lg text-lg w-96 px-6 py-4 flex justify-around mb-2 shadow-lg cursor-pointer mb-4`}
       href={link}
       target='_blank' rel='noreferrer'
     >
